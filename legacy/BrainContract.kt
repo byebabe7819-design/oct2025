b1e3000a2427bb0c7d1f47e31c5a4780666e2d50
byebabe7819-design <byebabe7819@gmail.com>
2025-10-18 00:43:00 +0000

    chore(clean): delete legacy sources folder

--- legacy/BrainContract.kt
+++
@@ -1,2 +0,0 @@
-// Original BrainContract moved to legacy folder
-// See canonical Kotlin implementations under app/src/main/kotlin/com/verumomnis/brains
--- legacy/SampleContradictionBrain.kt
+++
@@ -1,2 +0,0 @@
-// Original SampleContradictionBrain moved to legacy folder
-// See canonical Kotlin implementations under app/src/main/kotlin/com/verumomnis/brains
